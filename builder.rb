10463d9a7c4d4768a91179a5698e34b7fd866601
zxyle <zxyful@gmail.com>
2021-03-27 22:47:27 +0800

    修改homebrew脚本

--- builder.rb
+++ builder.rb
@@ -1,13 +1,13 @@
-class Asciinema < Formula
+class Builder < Formula
   include Language::Python::Virtualenv
 
-  desc "Record and share terminal sessions"
-  homepage "https://asciinema.org"
+  desc "Quickly generate scaffolding projects"
+  homepage "https://github.com/zxyle/builder"
   url "https://files.pythonhosted.org/packages/a7/71/771c859795e02c71c187546f34f7535487b97425bc1dad1e5f6ad2651357/asciinema-2.0.2.tar.gz"
   sha256 "32f2c1a046564e030708e596f67e0405425d1eca9d5ec83cd917ef8da06bc423"
-  license "GPL-3.0"
+  license "MIT"
   revision 3
-  head "https://github.com/asciinema/asciinema.git"
+  head "https://github.com/zxyle/builder.git"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "b681c70de003112e00b3c31555e06453e0d22483095713fd27cfe9113e5363c6"
