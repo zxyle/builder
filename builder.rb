33c4daa8b30ac0c593b62126974b45c5527b0862
zxyle <zxyful@gmail.com>
2021-03-29 18:51:51 +0800

    替换中文文件名

--- builder.rb
+++ builder.rb
@@ -3,29 +3,43 @@ class Builder < Formula
 
   desc "Quickly generate scaffolding projects"
   homepage "https://github.com/zxyle/builder"
-  url "https://files.pythonhosted.org/packages/a7/71/771c859795e02c71c187546f34f7535487b97425bc1dad1e5f6ad2651357/asciinema-2.0.2.tar.gz"
-  sha256 "32f2c1a046564e030708e596f67e0405425d1eca9d5ec83cd917ef8da06bc423"
+  url "https://github.com/zxyle/builder/archive/refs/tags/v0.0.1.zip"
+  sha256 "9da3976be38f9c64ebf41b007c6f2b43623b45b71a3d1c5c84f0edf1a788d80a"
   license "MIT"
   revision 3
   head "https://github.com/zxyle/builder.git"
 
+  livecheck do
+    url :stable
+  end
+
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b681c70de003112e00b3c31555e06453e0d22483095713fd27cfe9113e5363c6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "0a5101fb3da7411764b53fb8dc6b8ab8a7c4a54ced892f9c832301ecbb9964a0"
-    sha256 cellar: :any_skip_relocation, catalina:      "caa76523c644cf5916cde300c407d89282509a0291cfa9c4812d888c26ce7f77"
-    sha256 cellar: :any_skip_relocation, mojave:        "b1a1aff9cf3f46328f0df80773a85898db70453fff4785e26e7ee4d6f12ef408"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "4ce78d8edeeb635ad7cf2f5edd88770baaa72d623ecc471fb0e8f5e12efb0e04"
+    cellar :any
+    sha256 "08525974a3404b234d7de572fea0f6095c3e6749df9fc192d571b1f815f00f3d" => :big_sur
+    sha256 "f147a50464261bd34bd8af04390584badc4241e9d37ca54332fe7dac07e1d570" => :arm64_big_sur
+    sha256 "94142a948bb19d5f80bf22d7c3aab0778ef48a0900ba1d8fd454fa070de31844" => :catalina
+    sha256 "8cb911e8ce3f82f81c7fde523269118c9986d2524fb7f6b45137636c7b9c418c" => :mojave
   end
 
   depends_on "python@3.9"
 
+  resource "Jinja" do
+    url "https://files.pythonhosted.org/packages/df/50/70f41c599cf0ace6c554b07108916513c70d0e950b0abb18c27a865df7a2/Jinja-1.2.tar.gz"
+    sha256 "36252f3d0356008a31a537a31cfba189f7b5898b8e2da454f72ea5f987db8b31"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/6b/47/c14abc08432ab22dc18b9892252efaf005ab44066de871e72a38d6af464b/requests-2.25.1.tar.gz"
+    sha256 "27973dd4a904a4f13b263a19c866c13b92a39ed1c964655f025f3f8d3d75b804"
+  end
+
   def install
     virtualenv_install_with_resources
+    # bash_completion.install "contrib/completion/bash/docker-compose"
+    # zsh_completion.install "contrib/completion/zsh/_docker-compose"
   end
 
   test do
-    ENV["LC_ALL"] = "en_US.UTF-8"
-    output = shell_output("#{bin}/asciinema auth")
-    assert_match "Open the following URL in a web browser to link your install ID", output
+    system bin/"builder", "--help"
   end
 end
