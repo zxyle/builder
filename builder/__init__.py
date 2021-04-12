ce283ff6db7d35d619e00f74a860b989bb1b1b8f
zxyle <zxyful@gmail.com>
2021-04-12 22:03:05 +0800

    Update project metadata info

--- builder/__init__.py
+++ builder/__init__.py
@@ -6,10 +6,10 @@ __author__ = "Xiang Zheng"
 __email__ = "zxyful@gmail.com"
 __version__ = "0.0.1"
 __project__ = "builder"
-__description__ = "Quickly generate scaffolding projects"
+__description__ = "Quickly create batteries included project"
 __homepage__ = "https://github.com/zxyle/builder"
 __license__ = "MIT"
-__repository__ = "https://github.com/zxyle/builder.git"
+__repository__ = f"{__homepage__}.git"
 __python_version__ = f"{python_version[0]}.{python_version[1]}"
 
 if sys.version_info[0] < 3:
